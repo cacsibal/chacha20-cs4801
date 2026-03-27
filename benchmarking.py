39f2870247928dc58f1618bfabf7a30713b5d180
cacsibal <cacsibal@gmail.com>
2026-03-27 14:32:39 -0400

    benchmarking scaffolding

---
+++ benchmarking.py
@@ -0,0 +1,7 @@
+# import matplotlib
+
+def benchmark():
+    print('todo: compare encryption times against varying num_rounds, key lengths, plaintext lengths, etc')
+
+if __name__ == '__main__':
+    benchmark()
